b9f7d6d66a356fca6d3ec1f42e236772d7f03962
Aldi.BS <aldi.bluesilo@gmail.com>
2023-12-18 10:46:00 +0700

    update: punchOut validation

--- src/controllers/attendance.controller.js
+++ src/controllers/attendance.controller.js
@@ -65,6 +65,23 @@ class AttendancesController extends BaseController {
         throw customizeError(400, "Attendance record not found");
       }
 
+      const lastReturn =
+        attendanceRecord.breaks[attendanceRecord.breaks.length - 1];
+
+      if (!lastReturn) {
+        throw customizeError(
+          400,
+          "Cannot punch out. Last record doesn't have a break"
+        );
+      }
+
+      if (!lastReturn.returnFromBreak) {
+        throw customizeError(
+          400,
+          "Cannot punch out. Last record doesn't have a return from break"
+        );
+      }
+
       const punchInTime = attendanceRecord.punchIn;
       const punchOutTime = new Date();
       const totalHoursWorked = (punchOutTime - punchInTime) / (1000 * 60 * 60);
@@ -84,65 +101,6 @@ class AttendancesController extends BaseController {
       next(error);
     }
   }
-
-  // async punch_out(req, res, next) {
-  //   const { id } = req.params;
-
-  //   const upload = multer({
-  //     limits: {
-  //       fileSize: 1000000,
-  //     },
-  //     fileFilter(req, file, callback) {
-  //       if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-  //         return callback(new Error("Please upload an image"));
-  //       }
-
-  //       callback(undefined, true);
-  //     },
-  //   }).single("punchOutImage");
-
-  //   // Wrap the Multer middleware in a promise
-  //   const uploadMiddleware = () => {
-  //     return new Promise((resolve, reject) => {
-  //       upload(req, res, (err) => {
-  //         if (err) {
-  //           reject(err);
-  //         } else {
-  //           resolve();
-  //         }
-  //       });
-  //     });
-  //   };
-
-  //   try {
-  //     // Use async/await with the simplified uploadMiddleware
-  //     await uploadMiddleware();
-
-  //     const attendanceRecord = await AttendancesModel.findOne({ uId: id });
-
-  //     if (!attendanceRecord) {
-  //       throw customizeError(400, "Attendance record not found");
-  //     }
-
-  //     const punchInTime = attendanceRecord.punchIn;
-  //     const punchOutTime = new Date();
-  //     const totalHoursWorked = (punchOutTime - punchInTime) / (1000 * 60 * 60);
-
-  //     const status = totalHoursWorked > 8 ? "overtime" : "late";
-
-  //     attendanceRecord.punchOutDesc = req.body.punchOutDesc;
-  //     attendanceRecord.punchOutGps = req.body.punchOutGps;
-  //     attendanceRecord.punchOut = punchOutTime;
-  //     attendanceRecord.punchOutImage = req.file ? req.file.buffer : undefined;
-  //     attendanceRecord.status = status;
-
-  //     await attendanceRecord.save();
-
-  //     return res.status(200).json({ data: attendanceRecord });
-  //   } catch (error) {
-  //     next(error);
-  //   }
-  // }
 
   async break(req, res, next) {
     const { id } = req.params;
